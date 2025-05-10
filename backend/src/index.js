48a4018ff84aa48445b71b70c5640610a18edceb
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-05-10 23:10:05 +0530

    auth , and add problem form is added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import problemRoutes from "./routes/problem.routes.js";
@@ -9,28 +9,30 @@ import executionRoute from "./routes/executeCode.routes.js";
 import submissionRoutes from "./routes/submission.routes.js";
 import playlistRoutes from "./routes/playlist.routes.js";
 
-
 dotenv.config();
 
 const app = express();
 
+app.use(
+    cors({
+      origin: "http://localhost:5174",
+      credentials: true,
+    })
+  );
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/" , (req , res)=>{
-    res.send("Hello Guys welcome to leetlab🔥");
-})
+app.get("/", (req, res) => {
+  res.send("Hello Guys welcome to leetlab🔥");
+});
 
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/problems", problemRoutes);
+app.use("/api/v1/execute-code", executionRoute);
+app.use("/api/v1/submission", submissionRoutes);
 
-app.use("/api/v1/auth" , authRoutes);
-app.use("/api/v1/problems" , problemRoutes)
-app.use("/api/v1/execute-code" , executionRoute)
-app.use("/api/v1/submission" , submissionRoutes)
+app.use("/api/v1/playlist", playlistRoutes);
 
-app.use("/api/v1/playlist" , playlistRoutes)
-
-app.listen(process.env.PORT,()=>{
-    console.log("Server is running on port 8081");
-})
-
-
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port 8081");
+});
